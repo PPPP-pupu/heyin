97ba2a39b114d7ae6ed09d44533a941a183c5172
PP <a11@Mac.bbrouter>
2026-07-09 14:18:01 +0800

    fix: missing useEffect import in project page (breaking Vercel deployment)

--- src/app/project/[projectId]/page.tsx
+++ src/app/project/[projectId]/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useParams, useRouter } from "next/navigation";
 import AppShell from "@/components/common/AppShell";
 import AppHeader from "@/components/common/AppHeader";
